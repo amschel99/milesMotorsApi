f69abf63ba45950a348e02c4dc0aad512b1e5290
amschel99 <6290.2020@students.ku.ac.ke>
2022-11-10 16:43:02 +0300

    added the single route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDb from './db/config.js'
 import carsRouter from "./routes/cars.js"
+import carRouter from "./routes/car.js"
 import cors from 'cors'
 dotenv.config()
 
@@ -12,7 +13,7 @@ const app= express()
 const PORT= process.env.PORT || 8000
 
 app.get('/',(req,res)=>{
-res.send('hey loo...')
+res.send('Miles Motors Api ')
 })
 const start= async  ()=>{
 
@@ -25,6 +26,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use('/cars',carsRouter);
+app.use("/car",carRouter);
 
 app.listen(PORT, ()=>{
 console.log("server running on "+ PORT)
--- routes/car.js
+++ routes/car.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import {getSingleCar} from "../controllers/getSingleCar.js"
  const router= express.Router();
-router.route('/:carId').get().patch().delete()
+router.route('/:carId').get(getSingleCar).patch().delete()
 export default router
