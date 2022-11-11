165e1dc58511d588a22f7ad815ea2e3e5540b5b3
amschel99 <6290.2020@students.ku.ac.ke>
2022-11-11 22:27:19 +0300

    changed one

--- cars.js
+++ cars.js
@@ -1,4 +1,4 @@
-export const cars= [{"_id":"618c00a1c93959b38d56f27b","carID":1,"carName":"Toyota Succeed","carType":"Toyota","color":"white","transmission":"auto","fuel":"petrol","bodyStyle":"sedan","engine":"DOHC 24-valve V-6","mileage":35000,"price":1100000,"carImg":"/images/all-cars/bmw-m6.jpg",cubicCentimeters:"1500",image2:"https://thumbs2.imgbox.com/6f/ed/PdlC8STV_t.jpeg",image3:'https://thumbs2.imgbox.com/dc/4f/I3zEgCeJ_t.jpeg',image4:'https://thumbs2.imgbox.com/82/f8/5apoonvE_t.jpeg',image5:'https://thumbs2.imgbox.com/18/e4/aFm1VKVY_t.jpeg'}, 
+export const cars= [{"_id":"618c00a1c93959b38d56f27b","carID":1,"carName":"Toyota Succeed","carType":"Toyota","color":"white","transmission":"auto","fuel":"petrol","bodyStyle":"sedan","engine":"DOHC 24-valve V-6","mileage":35000,"price":1100000,"carImg":"https://thumbs2.imgbox.com/6f/ed/PdlC8STV_t.jpeg",cubicCentimeters:"1500",image2:"https://thumbs2.imgbox.com/6f/ed/PdlC8STV_t.jpeg",image3:'https://thumbs2.imgbox.com/dc/4f/I3zEgCeJ_t.jpeg',image4:'https://thumbs2.imgbox.com/82/f8/5apoonvE_t.jpeg',image5:'https://thumbs2.imgbox.com/18/e4/aFm1VKVY_t.jpeg'}, 
 
 
 
