7ebefc0b3ea8e82c4a9a334cad7eeaca0da0e60d
amschel99 <6290.2020@students.ku.ac.ke>
2022-11-12 03:24:26 +0300

    navbar changes

--- cars.js
+++ cars.js
@@ -1,6 +1,12 @@
 export const cars= [{"carID":0,"carName":"Toyota Succeed","carType":"Toyota","color":"white","transmission":"auto","fuel":"petrol","bodyStyle":"sedan","engine":"DOHC 24-valve V-6","mileage":35000,"price":1100000,"carImg":"https://thumbs2.imgbox.com/6f/ed/PdlC8STV_t.jpeg",cubicCentimeters:"1500",image2:"https://thumbs2.imgbox.com/6f/ed/PdlC8STV_t.jpeg",image3:'https://thumbs2.imgbox.com/dc/4f/I3zEgCeJ_t.jpeg',image4:'https://thumbs2.imgbox.com/82/f8/5apoonvE_t.jpeg',image5:'https://thumbs2.imgbox.com/18/e4/aFm1VKVY_t.jpeg'},
-{"carID":2,"carName":"Toyota Succeed","carType":"Toyota","color":"white","transmission":"auto","fuel":"petrol","bodyStyle":"sedan","engine":"DOHC 24-valve V-6","mileage":35000,"price":1100000,"carImg":"https://thumbs2.imgbox.com/6f/ed/PdlC8STV_t.jpeg",cubicCentimeters:"1500",image2:"https://thumbs2.imgbox.com/6f/ed/PdlC8STV_t.jpeg",image3:'https://thumbs2.imgbox.com/dc/4f/I3zEgCeJ_t.jpeg',image4:'https://thumbs2.imgbox.com/82/f8/5apoonvE_t.jpeg',image5:'https://thumbs2.imgbox.com/18/e4/aFm1VKVY_t.jpeg'}, 
-{"carID":3,"carName":"Toyota Succeed","carType":"Toyota","color":"white","transmission":"auto","fuel":"petrol","bodyStyle":"sedan","engine":"DOHC 24-valve V-6","mileage":35000,"price":1100000,"carImg":"https://thumbs2.imgbox.com/6f/ed/PdlC8STV_t.jpeg",cubicCentimeters:"1500",image2:"https://thumbs2.imgbox.com/6f/ed/PdlC8STV_t.jpeg",image3:'https://thumbs2.imgbox.com/dc/4f/I3zEgCeJ_t.jpeg',image4:'https://thumbs2.imgbox.com/82/f8/5apoonvE_t.jpeg',image5:'https://thumbs2.imgbox.com/18/e4/aFm1VKVY_t.jpeg'}, 
+
+
+
+{"carID":1,"carName":"Subaru Impreza","carType":"Subaru","color":"blue","transmission":"auto","fuel":"petrol","bodyStyle":"sedan","engine":"DOHC 24-valve V-6","mileage":35000,"price":535000,"carImg":"https://thumbs2.imgbox.com/31/2b/CkqxgmgL_t.jpeg",cubicCentimeters:"1500"}, 
+
+{"carID":2,"carName":"Toyota Cygnus","carType":"Toyota","color":"black","transmission":" 5-speed automatic","fuel":"petrol","bodyStyle":"sedan","engine":"DOHC 24-valve V-6","mileage":35000,"price":3000000,"carImg":"https://thumbs2.imgbox.com/ff/7d/qfUn4gHE_t.jpeg",cubicCentimeters:"650"}, 
+{"carID":3,"carName":"Daihatsu Mira","carType":"Audi","color":"white","transmission":"auto","fuel":"petrol","bodyStyle":"sedan","engine":"DOHC 24-valve V-6","mileage":35000,"price":750000,"carImg":"https://thumbs2.imgbox.com/b6/be/zlZhogxZ_t.jpg",cubicCentimeters:"4700"}, 
+
 
 
 
