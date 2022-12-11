37b1dd33919a7238da940aa8d83bd5c5dfce3484
amschel99 <6290.2020@students.ku.ac.ke>
2022-12-11 15:34:29 +0300

    added a console log

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,7 +15,7 @@ try{
     //hash the password
     const salt= await bcrypt.genSalt(10)
  
-    hashPassword= await bcrypt.hash(req.body.password,salt)
+    const hashPassword= await bcrypt.hash(req.body.password,salt)
    req.body.password=hashPassword
 
     //Check if the user exists in the database
