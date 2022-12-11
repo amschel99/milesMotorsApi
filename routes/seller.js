de2783b2264766d954e0d207b84371156c6a4fc8
amschel99 <6290.2020@students.ku.ac.ke>
2022-12-11 18:45:22 +0300

    added a console log

--- routes/seller.js
+++ routes/seller.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import {getSeller} from '../controllers/getSeller.js'
  const router= express.Router();
- router.route("/:_id").get(getSeller)
+ router.route("/:userId").get(getSeller)
 
 
 export default router
