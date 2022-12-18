8dcb1ed4ff3b822cf861044b0dda24f1770bc022
amschel99 <6290.2020@students.ku.ac.ke>
2022-12-18 11:01:15 +0300

    added limit size to body-parser

--- index.js
+++ index.js
@@ -7,30 +7,42 @@ import carRouter from "./routes/car.js"
 import auth from "./routes/auth.js"
 import seller from './routes/seller.js'
 import cors from 'cors'
+import bodyParser from 'body-parser'
 dotenv.config()
 
 const app= express()
 
 
 const PORT= process.env.PORT || 8000
+app.use( bodyParser.json({limit: '50mb'}) );
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true,
+  parameterLimit:50000
+}));
+app.use(cors({
+    origin: '*'
+}));
+
+app.use('/cars',carsRouter);
+app.use("/car",carRouter);
+app.use('/auth',auth);
+app.use('/seller', seller);
 
 app.get('/',(req,res)=>{
 res.send('All cars API  : developed by Amschel :-) ')
 })
+
+
 const start= async  ()=>{
 
 try{
 await connectDb(process.env.MONGO_URI,{
 
 })
-app.use(cors({
-    origin: '*'
-}));
-app.use(express.json());
-app.use('/cars',carsRouter);
-app.use("/car",carRouter);
-app.use('/auth',auth);
-app.use('/seller', seller);
+
+
+
 
 
 app.listen(PORT, ()=>{
