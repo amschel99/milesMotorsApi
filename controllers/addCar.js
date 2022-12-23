12435f23706c175a29d195777228630b0c24298a
amschel99 <6290.2020@students.ku.ac.ke>
2022-12-23 19:52:54 +0800

    added some models

--- controllers/addCar.js
+++ controllers/addCar.js
@@ -1,15 +1,16 @@
- import carsModel from '../models/cars.js'
- 
- export const addCar= async (req,res)=>{
+import carsModel from '../models/cars.js'
 
-    try{
+export const addCar = async (req, res) => {
 
-const car= await carsModel.create(req.body);
-console.log(`success`)
-  return res.status(201).json({"code":1})
-    }
-    catch(error){
-      console.log(error)
-return res.status(500).send(`there was an error ${error.message}`)
-    }
- }
+  try {
+
+
+    const car = await carsModel.create(req.body);
+    console.log(`success`)
+    return res.status(201).json({ "code": 1 })
+  }
+  catch (error) {
+    console.log(error)
+    return res.status(500).send(`there was an error ${error.message}`)
+  }
+}
